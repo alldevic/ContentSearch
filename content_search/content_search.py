541107dbd0891616b19b20d6b1037d9b1d77e8e1
Nikolay Bely <belyynikolay@gmail.com>
2019-06-30 02:33:51 +0700

    Add html tools and remove part of unneeded tags

--- content_search/content_search.py
+++ content_search/content_search.py
@@ -1,4 +1,7 @@
 import click
+import requests
+from bs4 import BeautifulSoup, Comment
+import html5lib
 
 
 @click.command()
@@ -9,8 +12,25 @@ def main(url, out):
     A simple content parser for URL.
     This tool will find content blocks and write out onformation to out file.
     """
-    print(url)
-    print(out)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'
+    }
+
+    response = requests.get(url, headers)
+
+    soup = BeautifulSoup(response.text, 'html5lib').body
+
+    ignored_tags = ['script', 'header', 'footer', 'style', 'noscript']
+    for tag in ignored_tags:
+        for entity in soup(tag):
+            entity.decompose()
+
+    comments = soup.find_all(string=lambda text: isinstance(text, Comment))
+    for comment in comments:
+        comment.extract()
+
+    with open(out, 'w') as output_file:
+        output_file.write(soup.prettify())
 
 
 if __name__ == "__main__":
